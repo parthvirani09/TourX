d3b41aea19516c8e04789d5e229f5b7ba7d64ff5
parthvirani09 <131760459+parthvirani09@users.noreply.github.com>
2023-10-15 11:23:49 +0530

    Add files via upload

---
+++ captown/cape-town.php
@@ -0,0 +1,256 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Cape-Town</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="initial-scale=1, width=device-width" />
+<link rel="shortcut icon" href="public\undefined1.png" type="image/x-icon">
+    <link rel="stylesheet" href="./global.css" />
+    <link rel="stylesheet" href="./cape-town.css" />
+    <link rel="stylesheet" href="hover2.css" />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,600;0,700;0,900;1,400&display=swap"
+    />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Architects Daughter:wght@400&display=swap"
+    />
+  </head>
+  <body>
+    <div class="cape-town">
+      <div class="lion" id="lionContainer">
+        <div class="card" style="width: 415px; height: 448px">
+          <span></span>
+          <div class="imgBx">
+            <img class="lion-child" alt="" src="./public/undefined30.png" />
+          </div>
+          <div class="content">
+            <div>
+              <h2>Lion's Head</h2>
+              <p>
+                Lion's Head in Cape Town is a peak situated at 669 meters above
+                sea level. The hike up Lions Head takes approximately 90 minutes
+                at a moderate pace. Lions Head trail is steep but do-able, while
+                the 5.5km trail packs a punch (Both ways).
+                <br /><br />
+                Location: Signal Hill Road, Cape Town Central
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="lions-head">
+          <p class="table-mountain-aerial">Lion's Head</p>
+        </div>
+        <img class="map-pin-icon" alt="" src="./public/undefined23.png" />
+      </div>
+      <div class="description">
+        <div class="cape-town1">Cape Town.</div>
+        <img class="map-pin-icon1" alt="" src="./public/undefined31.png" />
+
+        <div class="discover-cape-towns">
+          Discover Cape Town's beauty, where majestic Table Mountain meets
+          pristine beaches. Enjoy diverse cultures, world-class wine, and
+          thrilling adventures, making it an unforgettable destination for every
+          traveler. 🏞️🍷🏖️🇿🇦
+        </div>
+      </div>
+      <div class="navigation1">
+        <div class="logo4" id="logoContainer1">
+          <div class="logo5">
+            <img
+              class="bird-2-transformed-2-icon2"
+              alt=""
+              src="./public/undefined1.png"
+            />
+
+            <div class="tourx3">TourX</div>
+          </div>
+        </div>
+        <div class="nav1">
+          <div class="home1" id="homeText">Home</div>
+          <div class="home1" id="aboutText">About</div>
+          <div class="explore4" id="exploreText">Explore</div>
+          <!-- <a class="home" id="loginTex " href="login.php" style="text-decoration: none; ">Log out</a> -->
+        </div>
+      </div>
+      <div class="cable" id="cableContainer">
+        <div class="card" style="width: 400px; height: 449px">
+          <span></span>
+          <div class="imgBx">
+            <img class="cable-child" alt="" src="./public/undefined32.png" />
+          </div>
+          <div class="content">
+            <div>
+              <h2>Table Mountain Aerial Cableway</h2>
+              <p>
+                The best views of Cape Town are seen from the top of Table
+                Mountain. The Cableway takes you to the summit in under 5
+                minutes and the cable car's rotating floor ensure that all
+                visitors get a 360 degree aerial view of the city.
+                <br /><br />
+                Location: Tafelberg Road Lower Cable Station, Cape Town
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="table-mountain-aerial-container">
+          <p class="table-mountain-aerial">Table Mountain Aerial Cableway</p>
+        </div>
+        <img class="map-pin-icon2" alt="" src="./public/undefined23.png" />
+      </div>
+      <div class="nature" id="natureContainer">
+        <div class="card" style="width: 437px; height: 448px">
+          <span></span>
+          <div class="imgBx">
+            <img class="nature-child" alt="" src="./public/undefined33.png" />
+          </div>
+          <div class="content">
+            <div>
+              <h2>Cape Point Nature Reserve</h2>
+              <p>
+                At the tip of the Cape Peninsula, 60km SouthWest of Cape Town,
+                lies Cape Point. Located within the southern section of Table
+                Mountain National Park; a declared World Heritage site, as part
+                of the Cape Floral region.
+                <br /><br />
+                Location: Cape Peninsula, Simon's Town, Cape Town
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="cape-point-nature-container">
+          <p class="table-mountain-aerial">Cape Point Nature Reserve</p>
+        </div>
+        <img class="map-pin-icon3" alt="" src="./public/undefined23.png" />
+      </div>
+      <div class="aquarium" id="aquariumContainer">
+        <div class="card" style="width: 460px; height: 300px">
+          <span></span>
+          <div class="imgBx">
+            <img class="aquarium-child" alt="" src="./public/undefined34.png" />
+          </div>
+          <div class="content">
+            <div>
+              <h2>Two Oceans Aquarium</h2>
+              <p>
+                The Two Oceans Aquarium is a window into the spectacular oceans
+                surrounding the southern African coast. Through displays ranging
+                from the small and almost fantastical Knysna seahorses.
+                <br /><br />
+                Location: Dock Rd, Victoria & Alfred Waterfront, Simon's Town,
+                Cape Town
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="two-oceans-aquarium-container">
+          <p class="table-mountain-aerial">Two Oceans Aquarium</p>
+        </div>
+        <img class="map-pin-icon4" alt="" src="./public/undefined23.png" />
+      </div>
+      <div class="bo" id="boContainer">
+        <div class="card" style="width: 421px; height: 448px">
+          <span></span>
+          <div class="imgBx">
+            <img class="bo-child" alt="" src="./public/undefined35.png" />
+          </div>
+          <div class="content">
+            <div>
+              <h2>Bo-kaap</h2>
+              <p>
+                The Bo-Kaap (lit. "above the Cape" in Afrikaans) is an area of
+                Cape Town, South Africa formerly known as the Malay Quarter. It
+                is a former racially segregated area, situated on the slopes of
+                Signal Hill above the city centre and is a historical centre of
+                Cape Malay culture in Cape Town. The Nurul Islam Mosque,
+                established in 1844, is located in the area
+                <br /><br />
+                Location: Bo-KaapSchotsche Kloof, Cape Town
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="bo-kaap">
+          <p class="table-mountain-aerial">Bo-kaap</p>
+        </div>
+        <img class="map-pin-icon5" alt="" src="./public/undefined23.png" />
+      </div>
+      <div class="diamond" id="diamondContainer">
+        <div class="card" style="width: 478px; height: 300px">
+          <span></span>
+          <div class="imgBx">
+            <img class="diamond-child" alt="" src="./public/undefined36.png" />
+          </div>
+          <div class="content">
+            <div>
+              <h2>Cape Town Diamond Museum</h2>
+              <p>
+                The Cape Town Diamond Museum pays tribute to the world's most
+                precious gem, the diamond. Discover a 3 billion year old story
+                unfold from a diamond's formation to the final polished stone.
+                <br /><br />
+                Location: Level 1 Clock Tower, Victoria & Alfred Waterfront
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="cape-town-diamond-container">
+          <p class="table-mountain-aerial">Cape Town Diamond Museum</p>
+        </div>
+        <img class="map-pin-icon6" alt="" src="./public/undefined23.png" />
+      </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+    integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script>
+
+var logoContainer = document.getElementById("logoContainer1");
+      if (logoContainer) {
+        logoContainer.addEventListener("click", function () {
+          window.open("http://localhost/reg");
+        });
+      }
+
+      var homeText = document.getElementById("homeText");
+      if (homeText) {
+        homeText.addEventListener("click", function () {
+          window.open("http://localhost/reg");
+        });
+      }
+
+      var aboutText = document.getElementById("aboutText");
+      if (aboutText) {
+        aboutText.addEventListener("click", function () {
+          window.open("http://localhost/reg/#about_");
+        });
+      }
+
+      var exploreText = document.getElementById("exploreText");
+      if (exploreText) {
+        exploreText.addEventListener("click", function () {
+          window.open("http://localhost/reg/#explore_");
+        });
+      }
+
+      $(document).ready(function () {
+        $(".card").on("mouseenter", function (e) {
+          (x = e.pageX - $(this).offset().left),
+            (y = e.pageY - $(this).offset().top);
+          $(this).find("span").css({ top: y, left: x });
+        });
+        $(".card").on("mouseout", function (e) {
+          (x = e.pageX - $(this).offset().left),
+            (y = e.pageY - $(this).offset().top);
+          $(this).find("span").css({ top: y, left: x });
+        });
+      });
+    </script>
+  </body>
+</html>
---
+++ gene/g-e-n-e-v-a.php
@@ -0,0 +1,301 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Geneva</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="initial-scale=1, width=device-width" />
+    <link rel="shortcut icon" href="public\undefined1.png" type="image/x-icon">
+    <link rel="stylesheet" href="./global.css" />
+    <link rel="stylesheet" href="./g-e-n-e-v-a.css" />
+    <link rel="stylesheet" href="hover2.css" />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,600;0,700;0,900;1,400&display=swap"
+    />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Architects Daughter:wght@400&display=swap"
+    />
+  </head>
+  <body>
+    <div class="geneva">
+      <div class="navigation5">
+        <div class="logo12" id="logoContainer1">
+          <div class="logo13">
+            <img
+              class="bird-2-transformed-2-icon6"
+              alt=""
+              src="./public/undefined1.png"
+            />
+
+            <div class="tourx7">TourX</div>
+          </div>
+        </div>
+        <div class="nav5">
+          <div class="home5" id="homeText">Home</div>
+          <div class="home5" id="aboutText">About</div>
+          <div class="explore8" id="exploreText">Explore</div>
+          <!-- <a class="home" id="loginTex " href="login.php" style="text-decoration: none; ">Log out</a> -->
+        </div>
+      </div>
+      <img
+        class="icon-of-maldives3"
+        alt=""
+        src="./public/undefined301111.png"
+      />
+
+      <div class="geneva1">Geneva.</div>
+      <div class="discover-the-enchanting">
+        Discover the enchanting beauty of Switzerland: pristine alpine
+        landscapes, charming villages, world-class skiing, and exquisite
+        chocolates. Embrace adventure and culture in the heart of Europe's gem.
+        🏔️❄️🍫🇨🇭
+      </div>
+      <div class="rectangle-parent2">
+        <img class="group-child3" alt="" src="./public/undefined311111.png" />
+
+        <div class="switzerland2">Switzerland</div>
+      </div>
+      <div class="vector-parent" id="groupContainer2">
+        <div class="card" style="width: 700px; height: 281px">
+          <span></span>
+          <div class="imgBx">
+            <img
+              class="group-child4"
+              alt=""
+              src="./public/undefined321111.png"
+            />
+          </div>
+          <div class="content">
+            <div>
+              <h2>Musée Ariana</h2>
+              <p>
+                The Musée Ariana, also known as the Musée suisse de la céramique
+                et du verre (Swiss Museum of Ceramics and Glass), is a museum in
+                Geneva, Switzerland.
+                <br /><br />
+                Location: Av. de la Paix 10, 1202 Genève
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="scuba-parent">
+          <div class="scuba">
+            <p class="muse-ariana">Musée Ariana</p>
+          </div>
+          <img class="scuba-icon" alt="" src="./public/undefined331111.png" />
+        </div>
+      </div>
+      <div class="vector-group" id="groupContainer4">
+        <div class="card" style="width: 700px; height: 281px">
+          <span></span>
+          <div class="imgBx">
+            <img
+              class="group-child4"
+              alt=""
+              src="./public/undefined341111.png"
+            />
+          </div>
+          <div class="content">
+            <div>
+              <h2>Geneva City Tour</h2>
+              <p>
+                Enjoy an alternative sightseeing experience in Geneva, the city
+                hailed as Switzerland’s cultural capital, on a tour by coach,
+                tram, and boat. See the city’s most spectacular attractions from
+                a variety of vantage points and delve into Geneva’s rich history
+                with commentary from a professional guide.
+                <br /><br />
+                Location: From Keytours SA to St Pierre Cathedral
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="city-tour">
+          <div class="scuba-group">
+            <div class="scuba1">Geneva City Private Tour</div>
+            <img
+              class="scuba-icon1"
+              alt=""
+              src="./public/undefined351111.png"
+            />
+          </div>
+        </div>
+      </div>
+      <div class="rectangle-parent3" id="groupContainer6">
+        <div class="card" style="width: 307px; height: 472px">
+          <span></span>
+          <div class="imgBx">
+            <img class="child" alt="" src="./public/undefined361111.png" />
+          </div>
+          <div class="content">
+            <div>
+              <h2>To Chamonix from Geneva</h2>
+              <p>
+                The French Alps are tantalizingly close to Geneva, but getting
+                there can seem complicated. This day trip to the Mont Blanc town
+                of Chamonix offers a hassle-free day, offering private
+                round-trip transport and an itinerary that maximizes your time.
+                <br /><br />
+                Location: From Mont-Blanc to Mer de Glace
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="scuba-container">
+          <div class="scuba2">
+            <p class="muse-ariana">
+              From Geneva: Trip to Chamonix and Mont-Blanc
+            </p>
+          </div>
+          <img class="scuba-icon2" alt="" src="./public/undefined23.png" />
+        </div>
+      </div>
+      <div class="vector-container" id="groupContainer9">
+        <div class="card" style="width: 307px; height: 472px">
+          <span></span>
+          <div class="imgBx">
+            <img class="child" alt="" src="./public/undefined37111.png" />
+          </div>
+          <div class="content">
+            <div>
+              <h2>Chocolate Flavors Walking Tour of Geneva</h2>
+              <p>
+                Immerse yourself in Switzerland’s chocolate culture on a tour
+                that shares the chocolate, sights, and stories of Geneva. With
+                so many chocolate shops to choose from throughout the city.
+                <br /><br />
+                Location: At fountain to the left of the entrance of Starbucks
+                Quai des Bergues 23, 1201 Genève
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="choco">
+          <div class="choco-child"></div>
+          <div class="choco-inner">
+            <div class="scuba-parent1">
+              <div class="scuba3">Chocolate Flavors Walking Tour of Geneva</div>
+              <img class="scuba-icon3" alt="" src="./public/undefined23.png" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="div" id="container">
+        <div class="card" style="width: 307px; height: 472px">
+          <span></span>
+          <div class="imgBx">
+            <img class="child" alt="" src="./public/undefined38111.png" />
+          </div>
+          <div class="content">
+            <div>
+              <h2>Conservatoire et Jardin...</h2>
+              <p>
+                It was founded in 1817 in a former area of Bastions Park in 1817
+                by Augustin Pyramus de Candolle. The Botanical Gardens were
+                transferred to the Console site (192 rue de Lausanne) in 1904.
+                <br /><br />
+                Location: Chem. de l'Impératrice 1, 1292 Pregny-Chambésy
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="choco1">
+          <div class="choco-inner1">
+            <div class="choco-inner1">
+              <div class="scuba3">
+                <p class="muse-ariana">
+                  Conservatoire et Jardin botaniques de Genève
+                </p>
+              </div>
+              <img class="scuba-icon3" alt="" src="./public/undefined23.png" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="rectangle-parent4" id="groupContainer13">
+        <div class="card" style="width: 306px; height: 513px">
+          <span></span>
+          <div class="imgBx">
+            <img
+              class="group-child8"
+              alt=""
+              src="./public/undefined39111.png"
+            />
+          </div>
+          <div class="content">
+            <div>
+              <h2>
+                Full-Day Trip to Diablerets and Glacier 3000 from Montreux
+              </h2>
+              <p>
+                Travel high into the Swiss Alps on a day trip from Montreux, and
+                discover the mountain town of Les Diablerets at your own pace.
+                
+                <br /><br />
+                Location: -
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="boat">
+          <div class="scuba-parent3">
+            <div class="scuba5">
+              Full-Day Trip to Riviera Col du Pillon & Glacier 3000
+            </div>
+            <img class="scuba-icon5" alt="" src="./public/undefined23.png" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+    integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script>
+      var logoContainer = document.getElementById("logoContainer1");
+      if (logoContainer) {
+        logoContainer.addEventListener("click", function () {
+          window.open("http://localhost/reg");
+        });
+      }
+
+      var homeText = document.getElementById("homeText");
+      if (homeText) {
+        homeText.addEventListener("click", function () {
+          window.open("http://localhost/reg");
+        });
+      }
+
+      var aboutText = document.getElementById("aboutText");
+      if (aboutText) {
+        aboutText.addEventListener("click", function () {
+          window.open("http://localhost/reg/#about_");
+        });
+      }
+
+      var exploreText = document.getElementById("exploreText");
+      if (exploreText) {
+        exploreText.addEventListener("click", function () {
+          window.open("http://localhost/reg/#explore_");
+        });
+      }
+    $(document).ready(function () {
+      $('.card').on('mouseenter', function (e) {
+        x = e.pageX - $(this).offset().left,
+          y = e.pageY - $(this).offset().top;
+        $(this).find('span').css({ top: y, left: x });
+      })
+      $('.card').on('mouseout', function (e) {
+        x = e.pageX - $(this).offset().left,
+          y = e.pageY - $(this).offset().top;
+        $(this).find('span').css({ top: y, left: x });
+      })
+    })
+      </script>
+  </body>
+</html>
