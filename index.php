3e5ece18718d65c9f96b9e7e7c3c834c002887e0
parthvirani09 <131760459+parthvirani09@users.noreply.github.com>
2023-10-15 11:28:32 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,353 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="initial-scale=1, width=device-width" />
+<title>TourX</title>
+
+    <link rel="stylesheet" href="./global.css" />
+    <link rel="stylesheet" href="./index.css" />
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,600;0,700;1,400&display=swap"
+    />
+    <link rel="shortcut icon" href="public\undefined1.png" type="image/x-icon">
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Architects Daughter:wght@400&display=swap"
+    />
+  </head>
+  <body>
+    <div class="introduction">
+      <div class="introduction1">
+        <div class="introduction1">
+          <div class="homepage">
+            <video src="hdr.mp4" autoplay muted loop></video>
+            <!-- <img class="home-bg-2-icon" alt="" src="./public/undefined.png" /> -->
+
+            <div class="navigation">
+              <div class="logo">
+                <div class="logo1">
+                  <img
+                    class="bird-2-transformed-2-icon"
+                    alt=""
+                    src="./public/undefined1.png"
+                  />
+
+                  <div class="tourx" id="tourXText">TourX</div>
+                </div>
+              </div>
+              <div class="nav">
+                <div class="home" id="homeText" style="text-decoration: none; ">Home</div>
+                <a href="#about_" style="text-decoration: none; color: white;">
+                  <div class="home" id="aboutText">About</div></a>
+                  <a href="#explore_" style="text-decoration: none; color: white;">
+                <div class="explore" id="exploreText">Explore</div></a>
+                <!-- <div class="home">Hotel</div> -->
+                <a class="home" id="loginTex " href="login.php" style="text-decoration: none; ">Log out</a>
+              </div>
+            </div>
+            <div class="content">
+              <div class="welcome-to-travel">Welcome To Travel</div>
+              <div class="explore-the-world-container">
+                <p class="the-world">Explore</p>
+                <p class="the-world">The World.</p>
+              </div>
+              <div class="life-is-not">
+                Life is not only earning money. You must know how to spend too.
+                So here we are to help you with our travel.
+              </div>
+              <div class="start">
+                <a href="#explore_" style="text-decoration: none; color: white;">
+                <div class="start-child" id="rectangle"></div>
+                <div class="start-your-journey">Start Your Journey</div>
+                <img
+                  class="forward-button-icon"
+                  alt=""
+                  src="./public/undefined2.png"
+                />
+              </a>
+              </div>
+            </div>
+          </div>
+          <div id="about_" class="about1">
+            <div class="about2" >About</div>
+            <div class="tourx1">TourX</div>
+            <div class="about-child"></div>
+            <div class="about-item"></div>
+            <img class="about-inner" alt="" src="./public/undefined3.png" />
+
+            <div class="charusat-nadiad">charusat, Nadiad</div>
+            <i  class="welcome-to-tourx"
+              >Welcome to TourX, your passport to extraordinary travel
+              experiences! We are a passionate team of adventurers who believe
+              that the world is meant to be explored, one destination at a time.
+              Whether you're a seasoned globetrotter or embarking on your first
+              journey, TourX is here to inspire and assist you in creating
+              unforgettable memories. 🗺️📸</i>
+            <div class="darsh-vaishnani">
+              <p class="the-world">Darsh</p>
+              <p class="the-world">Vaishnani</p>
+            </div>
+            <div class="parth-virani">
+              <p class="the-world">Parth</p>
+              <p class="the-world">Virani</p>
+            </div>
+            <img class="rectangle-icon" alt="" src="./public/undefined4.png" />
+
+            <img class="darsh-pin-icon" alt="" src="./public/undefined5.png" />
+
+            <div class="charusat-nadiad1">charusat, Nadiad</div>
+            <a href="https://www.linkedin.com/in/darsh-vaishnani/">
+            <img
+              class="linkedin-darsh-icon"
+              alt=""
+              src="./public/undefined6.png"
+              id="linkedinDarshIcon"
+            /></a>
+
+            <img
+              class="github-parth-icon"
+              alt=""
+              src="./public/undefined7.png"
+              id="githubParthIcon"
+            />
+            <a href="https://github.com/DARSHVAISHNANI">
+            <img
+              class="github-darsh-icon"
+              alt=""
+              src="./public/undefined7.png"
+              id="githubDarshIcon"
+            />
+            </a>
+
+            <img class="parth-map-pin" alt="" src="./public/undefined8.png" />
+
+            <a href="https://www.linkedin.com/in/parth-virani-5ab074251/">
+            <img
+              class="parth-linkedin-icon"
+              alt=""
+              src="./public/undefined6.png"
+              id="parthLinkedinIcon"
+            />
+            </a>
+
+            <a href="https://www.instagram.com/darsh_vaishnani/">
+            <img
+              class="darsh-ins-icon"
+              alt=""
+              src="./public/undefined9.png"
+              id="darshInsIcon"
+            />
+          </a>
+
+          <a href="https://www.instagram.com/parth___virani/">
+            <img
+              class="parth-ins-icon"
+              alt=""
+              src="./public/undefined10.png"
+              id="parthInsIcon"
+            />
+            </a>
+
+            <img class="image-1-icon" alt="" src="./public/undefined11.png" />
+          </div>
+          <div class="explore2">
+            <div class="fix">
+            <div class="rectangle-parent">
+              <a href="rome\rome.php">
+              <img class="group-child" alt="" src="./public/undefined12.png" /></a>
+
+              <div class="italy">
+                <div class="itlay">Itlay</div>
+                <img
+                  class="spain-map-pin"
+                  alt=""
+                  src="./public/undefined13.png"
+                />
+              </div>
+            </div>
+            <div class="rectangle-group">
+              <a href="gene\g-e-n-e-v-a.php">
+              <img class="group-item" alt="" src="./public/undefined14.png" /></a>
+
+              <div class="switzerland">
+                <div class="switzerland1">Switzerland</div>
+                <img
+                  class="spain-map-pin1"
+                  alt=""
+                  src="./public/undefined15.png"
+                />
+              </div>
+            </div>
+            <div class="rectangle-container">
+              <a href="m\m-onte-carlo.php">
+              <img class="group-inner" alt="" src="./public/undefined16.png" /></a> 
+
+              <div class="italy1">
+                <div class="monaco-parent">
+                  <div class="monaco">Monaco</div>
+                  <img
+                    class="spain-map-pin2"
+                    alt=""
+                    src="./public/undefined17.png"
+                  />
+                </div>
+              </div>
+            </div>
+            <div class="group-div">
+              <a href="mal\maldives.php">
+              <img class="group-child" alt="" src="./public/undefined18.png" /></a>
+
+              <div class="maldives">Maldives</div>
+              <img
+                class="spain-map-pin3"
+                alt=""
+                src="./public/undefined19.png"
+              />
+            </div>
+          
+            <div class="sapin-parent">
+              <a href="barcelona\barcelona.php">
+              <img  class="sapin-icon"   alt="" src="./public/undefined20.png" />
+
+              <img
+                class="maldives-map-pin"
+                alt=""
+                src="./public/undefined21.png"
+              />
+
+              <div class="spain">Spain</div>
+            </div>
+            <a href="captown\cape-town.php">
+            <img class="explore-child" alt="" src="./public/undefined22.png" /></a>
+
+            <div class="soutn-afirca">Soutn Afirca</div>
+            <img
+              class="capto-loca-icon"
+              alt=""
+              src="./public/undefined23.png"
+            />
+          </div>
+
+            <div id="explore_" class="places-to-explore-container">
+              <div class="places">Places</div>
+              <div class="sarakhu">
+                <span class="to">To <span class="explore3">Explore!</span></span>
+              </div>
+            </div>
+            <div class="explore-item"></div>
+          </div>
+        </div>
+        <div class="footer">
+          <div class="journey-beyond-boundarise">
+            journey beyond boundarise.
+          </div>
+          <div class="footer-child"></div>
+          <div class="tourx-all-rights-container">
+            ©2023 TourX® .All Rights Reserved.
+            <a
+              class="privacy"
+              href="https://www.intuit.com/privacy/statement"
+              target="_blank"
+            >
+              <span class="privacy1">Privacy</span>
+            </a>
+            |
+            <a
+              class="privacy"
+              href="https://mailchimp.com/legal/terms/"
+              target="_blank"
+            >
+              <span class="privacy1">Terms</span>
+            </a>
+            |
+            <a
+              class="privacy"
+              href="https://mailchimp.com/legal/"
+              target="_blank"
+            >
+              <span class="privacy1">Legal</span>
+            </a>
+            |
+            <a
+              class="privacy"
+              href="https://mailchimp.com/legal/cookies/#optanon-toggle-display/"
+              target="_blank"
+            >
+              <span class="privacy1">Cookie Preferences</span>
+            </a>
+          </div>
+          <img
+            class="facebook-icon"
+            alt=""
+            src="./public/undefined24.png"
+            id="facebookIcon"
+          />
+
+          <div class="about-us">About Us</div>
+          <div class="resources">Resources</div>
+          <div class="partners">Partners</div>
+          <div class="contact">contact</div>
+          <div class="faqs">FAQs</div>
+          <div class="support">Support</div>
+          <div class="blog">Blog</div>
+          <div class="events">Events</div>
+          <div class="help-center">Help Center</div>
+          <div class="terms2">Terms</div>
+          <div class="privacy2">Privacy</div>
+          <div class="security">Security</div>
+          <div class="sitemap">Sitemap</div>
+          <div class="affiliate">Affiliate</div>
+          <div class="careers">Careers</div>
+          <div class="press">Press</div>
+          <div class="investors">Investors</div>
+          <div class="terms-of-use">Terms of use</div>
+          <div class="logo2" id="logoContainer3">
+            <div class="logo3">
+              <img
+                class="bird-2-transformed-2-icon1"
+                alt=""
+                src="./public/undefined25.png"
+              />
+
+              <div class="tourx2">TourX</div>
+            </div>
+          </div>
+          <img class="instagram-icon" alt="" src="./public/undefined26.png" />
+
+          <img class="twitterx-icon" alt="" src="./public/undefined27.png" />
+
+          <img class="youtube-icon" alt="" src="./public/undefined28.png" />
+
+          <img class="linkedin-icon" alt="" src="./public/undefined29.png" />
+        </div>
+      </div>
+    </div>
+
+    <script>
+      var tourXText = document.getElementById("tourXText");
+    if (tourXText) {
+      tourXText.addEventListener("click", function () {
+        window.open("http://localhost/reg/index.php");
+      });
+    }
+
+      var homeText = document.getElementById("homeText");
+      if (homeText) {
+        homeText.addEventListener("click", function () {
+          window.open("http://localhost/reg/index.php");
+        });
+      }
+    
+      
+      var logoContainer3 = document.getElementById("logoContainer3");
+      if (logoContainer3) {
+        logoContainer3.addEventListener("click", function () {
+          window.open("http://localhost/reg");
+        });
+      }
+      </script>
+  </body>
+</html>
