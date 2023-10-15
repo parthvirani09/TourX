2493923b87ec83f83633e492ea63086b4a07a0fb
parthvirani09 <131760459+parthvirani09@users.noreply.github.com>
2023-10-15 11:21:39 +0530

    Add files via upload

---
+++ barcelona/barcelona.php
@@ -0,0 +1,267 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Barcelona</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="initial-scale=1, width=device-width" />
+    <link rel="shortcut icon" href="public\undefined1.png" type="image/x-icon">
+    <link rel="stylesheet" href="./global.css" />
+    <link rel="stylesheet" href="./barcelona.css" />
+    <link rel="stylesheet" href="hover2.css" />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,600;0,700;0,900;1,400&display=swap"
+    />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Architects Daughter:wght@400&display=swap"
+    />
+  </head>
+
+  <body>
+    <div class="barcelona">
+      <div class="navigation2">
+        <div class="logo6">
+          <div class="logo7" id="logoContainer">
+            <img
+              class="bird-2-transformed-2-icon3"
+              alt=""
+              src="./public/undefined1.png"
+            />
+
+            <div class="tourx4">TourX</div>
+          </div>
+        </div>
+        <div class="nav2">
+          <!-- <a href="index.html"> -->
+          <div class="home2" id="homeText">Home</div>
+          <div class="home2" id="aboutText">About</div>
+          <div class="explore5" id="exploreText">Explore</div>
+          <!-- <a class="home" id="loginTex " href="login.php" style="text-decoration: none; ">Log out</a> -->
+        </div>
+      </div>
+      <div class="map-pin"></div>
+      <img class="icon-of-maldives" alt="" src="./public/undefined301.png" />
+
+      <div class="barcelona1">Barcelona.</div>
+      <div class="experience-the-unique">
+        Experience the unique fusion of Barcelona's rich culture, breathtaking
+        architecture, and vibrant street life. From Gaudi's masterpieces to
+        tapas delights, immerse yourself in this lively Spanish gem. 🏰🍷🏖️🇪🇸
+      </div>
+      <div class="hotel-parent" id="groupContainer">
+        <div class="card" style="width: 300px; height: 525px">
+          <span></span>
+          <div class="imgBx">
+            <img class="hotel-icon" alt="" src="./public/undefined311.png" />
+          </div>
+          <div class="content">
+            <div>
+              <h2>Basílica de la Sagrada Familia</h2>
+              <p>
+                The Basilica de la Sagrada Familia is a church devoted to the
+                Holy Family. One of Antoni Gaudi’s most famous works, the church
+                is perhaps best known for still being under construction since
+                1882 with works funded purely by donations.
+                <br /><br />
+                Location: C/ de Mallorca, 401, 08013
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="baslica-de-la-container">
+          <p class="baslica-de-la">Basílica de la Sagrada Familia</p>
+        </div>
+        <img class="hotel-icon1" alt="" src="./public/undefined321.png" />
+      </div>
+      <div class="hotel-group" id="groupContainer1">
+        <div class="card" style="width: 264.98px; height: 525px">
+          <span></span>
+          <div class="imgBx">
+            <img class="hotel-icon2" alt="" src="./public/undefined331.png" />
+          </div>
+          <div class="content">
+            <div>
+              <h2>Casa Batlló</h2>
+              <p>
+                Welcome to Barcelona's magical house. A Gaudí masterpiece. A
+                unique immersive experience. International Exhibition of the
+                Year 2022. Children free up to 12 years old.
+                <br /><br />
+                Location: Pg. de Gràcia, 43, 08007 Barcelona
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="casa-batll">
+          <p class="baslica-de-la">Casa Batlló</p>
+        </div>
+        <img class="hotel-icon3" alt="" src="./public/undefined341.png" />
+      </div>
+      <div
+        class="card"
+        style="top: 396px; left: 880px; width: 300px; height: 525px"
+      >
+        <span></span>
+        <div class="imgBx">
+          <img
+            class="barcelona-child"
+            alt=""
+            src="./public/undefined351.png"
+            id="group"
+          />
+        </div>
+        <div class="content">
+          <div>
+            <h2>Palace of Catalan Music</h2>
+            <p>
+              The Palau, an icon of modernist architecture in downtown Barcelona
+              The Palau de la Música Catalana is one of the most representative
+              monuments of the city.
+              <br /><br />
+              Location: C/ Palau de la Música, 4-6, 08003 Barcelona
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="hotel-container" id="groupContainer2">
+        <div class="card" style="width: 300px; height: 525px">
+          <span></span>
+          <div class="imgBx">
+            <img class="hotel-icon" alt="" src="./public/undefined361.png" />
+          </div>
+          <div class="content">
+            <div>
+              <h2>Parc Guell</h2>
+              <p>
+                The quirky colorful mosaics and undulating curves of Park Güell
+                are a signature of the legendary Catalan architect Antoni Gaudi,
+                who designed many of the features in this park.
+                <br /><br />
+                Location: Carrer d'Olot,5,08024 Barcelona
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="parc-guell">
+          <p class="baslica-de-la">Parc Guell</p>
+        </div>
+        <img class="hotel-icon5" alt="" src="./public/undefined23.png" />
+      </div>
+      <div class="palace-of-catalan-container">
+        <p class="baslica-de-la">Palace of Catalan Music</p>
+      </div>
+      <img class="hotel-icon6" alt="" src="./public/undefined23.png" />
+      <div
+        class="card"
+        style="width: 582px; height: 227px; top: -400px; left: 530px"
+      >
+        <span></span>
+        <div class="imgBx">
+          <img
+            class="barcelona-item"
+            alt=""
+            src="./public/undefined37.png"
+            id="rectangleImage"
+          />
+        </div>
+        <div class="content">
+          <div>
+            <h2>Gothic Quarter</h2>
+            <p>
+              With its iconic, old-Europe streets and alleys, this picturesque
+              neighbourhood is the very heart of Barcelona.
+              <br /><br />
+              Location: Ciutat Vella district.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="gothic-quarter-barri-container">
+        <p class="baslica-de-la">Gothic Quarter (Barri Gotic)</p>
+      </div>
+      <img class="hotel-icon7" alt="" src="./public/undefined38.png" />
+
+      <img class="barcelona-inner" alt="" src="./public/undefined39.png" />
+
+      <div class="spain1">Spain</div>
+      <div
+        class="card"
+        style="width: 430px; height: 797px; top: -630px; left: 64px"
+      >
+        <span></span>
+        <div class="imgBx">
+          <img
+            class="barcelona-child1"
+            alt=""
+            src="./public/undefined40.png"
+            id="rectangleImage2"
+          />
+        </div>
+        <div class="content">
+          <div>
+            <h2>Spotify Camp Nou</h2>
+            <p>
+              This gigantic stadium is the largest soccer stadium in Europe,
+              with a seating capacity of 100,000
+              <br /><br />
+              Location: Carrer d'Aristides Maillol
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="spotify-camp-nou">Spotify Camp Nou</div>
+      <img class="hotel-icon8" alt="" src="./public/undefined23.png" />
+    </div>
+  <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+    integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+  <script>
+   var logoContainer = document.getElementById("logoContainer");
+      if (logoContainer) {
+        logoContainer.addEventListener("click", function () {
+          window.open("http://localhost/reg");
+        });
+      }
+
+      var homeText = document.getElementById("homeText");
+      if (homeText) {
+        homeText.addEventListener("click", function () {
+          window.open("http://localhost/reg");
+        });
+      }
+
+      var aboutText = document.getElementById("aboutText");
+      if (aboutText) {
+        aboutText.addEventListener("click", function () {
+          window.open("http://localhost/reg/#about_");
+        });
+      }
+
+      var exploreText = document.getElementById("exploreText");
+      if (exploreText) {
+        exploreText.addEventListener("click", function () {
+          window.open("http://localhost/reg/#explore_");
+        });
+      }
+    $(document).ready(function () {
+      $('.card').on('mouseenter', function (e) {
+        x = e.pageX - $(this).offset().left,
+          y = e.pageY - $(this).offset().top;
+        $(this).find('span').css({ top: y, left: x });
+      })
+      $('.card').on('mouseout', function (e) {
+        x = e.pageX - $(this).offset().left,
+          y = e.pageY - $(this).offset().top;
+        $(this).find('span').css({ top: y, left: x });
+      })
+    })
+  </script>
+</body>
+
+</html>
